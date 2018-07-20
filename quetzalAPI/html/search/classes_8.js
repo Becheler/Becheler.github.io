232c3203f27ceae2ce1b8b81945f044b306df783
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2018-07-20 10:52:07 -0400

    html regenerated

--- quetzalAPI/html/search/classes_8.js
+++ quetzalAPI/html/search/classes_8.js
@@ -1,9 +1,8 @@
 var searchData=
 [
   ['iddc_5fmodel_5f1',['IDDC_model_1',['../classquetzal_1_1simulators_1_1_i_d_d_c__model__1.html',1,'quetzal::simulators']]],
-  ['identity',['identity',['../structquetzal_1_1abc_1_1identity.html',1,'quetzal::abc::identity&lt; T &gt;'],['../structquetzal_1_1coalescence_1_1occupancy__spectrum_1_1_identity.html',1,'quetzal::coalescence::occupancy_spectrum::Identity']]],
+  ['identity',['Identity',['../structquetzal_1_1coalescence_1_1occupancy__spectrum_1_1_identity.html',1,'quetzal::coalescence::occupancy_spectrum::Identity'],['../structquetzal_1_1abc_1_1identity.html',1,'quetzal::abc::identity&lt; T &gt;']]],
   ['in_5fmemoized_5fdistribution',['in_memoized_distribution',['../structquetzal_1_1coalescence_1_1occupancy__spectrum_1_1in__memoized__distribution.html',1,'quetzal::coalescence::occupancy_spectrum']]],
-  ['individual_5fbased',['individual_based',['../structquetzal_1_1demography_1_1strategy_1_1individual__based.html',1,'quetzal::demography::strategy']]],
   ['init_5flibrary',['init_library',['../structgdalcpp_1_1detail_1_1init__library.html',1,'gdalcpp::detail']]],
   ['init_5fwrapper',['init_wrapper',['../structgdalcpp_1_1detail_1_1init__wrapper.html',1,'gdalcpp::detail']]],
   ['inversepowerlaw',['InversePowerLaw',['../structquetzal_1_1demography_1_1dispersal_1_1_inverse_power_law.html',1,'quetzal::demography::dispersal']]]

41a906c13de84e9a73d6a4eef942f7057599cd60
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2019-09-18 11:45:46 -0400

    html regenerated

--- quetzalAPI/html/modules.js
+++ quetzalAPI/html/modules.js
@@ -3,8 +3,8 @@ var modules =
     [ "geography", "group__geography.html", "group__geography" ],
     [ "expressive", "group__expressive.html", "group__expressive" ],
     [ "demography", "group__demography.html", "group__demography" ],
+    [ "simulator", "group__simulator.html", "group__simulator" ],
     [ "random", "group__random.html", "group__random" ],
     [ "coalescence", "group__coalescence.html", "group__coalescence" ],
-    [ "simulator", "group__simulator.html", "group__simulator" ],
     [ "abc", "group__abc.html", "group__abc" ]
 ];

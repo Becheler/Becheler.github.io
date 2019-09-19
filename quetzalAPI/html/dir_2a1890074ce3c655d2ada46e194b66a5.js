6a8a212b27968cda1fe7a46edc6dbb214ddcfa22
Arnaud Becheler <Arnaud.Becheler@egce.cnrs-gif.fr>
2019-09-19 14:14:30 -0400

    html regenerated

--- quetzalAPI/html/dir_2a1890074ce3c655d2ada46e194b66a5.js
+++ quetzalAPI/html/dir_2a1890074ce3c655d2ada46e194b66a5.js
@@ -1,7 +1,5 @@
 var dir_2a1890074ce3c655d2ada46e194b66a5 =
 [
-    [ "simulator.h", "modules_2simulator_2simulator_8h.html", [
-      [ "DiscreteTimeWrightFisher", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher.html", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher" ]
-    ] ],
+    [ "simulator.h", "modules_2simulator_2simulator_8h.html", null ],
     [ "utils.h", "modules_2simulator_2utils_8h.html", "modules_2simulator_2utils_8h" ]
 ];
--- quetzalAPI/html/group__simulator.js
+++ quetzalAPI/html/group__simulator.js
@@ -16,5 +16,12 @@ var group__simulator =
       [ "make_forest_and_coalesce_along_spatial_history", "classquetzal_1_1simulator_1_1_spatially_explicit_af788370982375493a5c791677972a1b3.html#af788370982375493a5c791677972a1b3", null ],
       [ "make_forest_using_sampling_time_and_coalesce_along_spatial_history", "classquetzal_1_1simulator_1_1_spatially_explicit_a7493b4081d0d0019bc4ec0f095911cf3.html#a7493b4081d0d0019bc4ec0f095911cf3", null ],
       [ "pop_size_history", "classquetzal_1_1simulator_1_1_spatially_explicit_afddcbb48a4ccc35a1d7eb180a206a830.html#afddcbb48a4ccc35a1d7eb180a206a830", null ]
+    ] ],
+    [ "DiscreteTimeWrightFisher", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher.html", [
+      [ "forest_type", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher_a43f157e6e8931d3b7c2030ad717b0e8b.html#a43f157e6e8931d3b7c2030ad717b0e8b", null ],
+      [ "coalesce", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher_af86eef8547437e13257965385f2cebbb.html#af86eef8547437e13257965385f2cebbb", null ],
+      [ "coalesce", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher_a726e4fdab5ad5f02c2705a5d67d78cc0.html#a726e4fdab5ad5f02c2705a5d67d78cc0", null ],
+      [ "coalesce", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher_a3c3c7c6fc1eaa435c9b606f57d726c15.html#a3c3c7c6fc1eaa435c9b606f57d726c15", null ],
+      [ "sample_waiting_time", "classquetzal_1_1simulator_1_1_discrete_time_wright_fisher_a6ede7415063f80f8a07120047f738814.html#a6ede7415063f80f8a07120047f738814", null ]
     ] ]
 ];
